312422d296d529312948acd4deb586a96218d174
Matheus Damasceno <matheusdamascenotx@gmail.com>
2019-07-04 10:49:15 -0300

    Adding CameraViewController
    Remove storyboard, added the cameraviewcontroller and methods for camera usage

--- PokedexMEL/AppDelegate.swift
+++ PokedexMEL/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window?.rootViewController = CameraViewController()
+        self.window?.makeKeyAndVisible()
         return true
     }
 
