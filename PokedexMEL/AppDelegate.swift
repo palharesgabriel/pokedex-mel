cbb6185d46f553c1a07a05297ce0baf662b67509
Gabriel Palhares <gaapalhares@gmail.com>
2019-07-04 12:03:24 -0300

    renan

--- PokedexMEL/AppDelegate.swift
+++ PokedexMEL/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         self.window = UIWindow(frame: UIScreen.main.bounds)
         
-        window?.rootViewController = PokemonLinsViewController()
+        window?.rootViewController = CameraViewController()
         self.window?.makeKeyAndVisible()
         return true
     }
